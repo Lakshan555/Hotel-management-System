3efbb659b0e0a5c103acb9b149bcf854652da49e
D.I.P Lakshan <it19951836@my.sliit,lk>
2021-09-24 12:44:18 +0530

    customerRoutes add

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use('/api/supplierorder', SupplierOrderRoutes);
 app.use(attendRoutes);
 app.use('/api/stock', StockRoutes);
 app.use(eventRoutes);
+app.use(customerRoutes);
 
 const PORT = 8000;// sever port
 const DB_URL = `mongodb+srv://Admin:admin321@project.0tb9c.mongodb.net/HTM_Db?retryWrites=true&w=majority`;
